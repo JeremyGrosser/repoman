a9b4de203561476b5b761fe6f434b102762100f2
Jeremy Grosser <synack@digg.com>
2009-10-09 13:39:20 -0700

    New version number and updated example config

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='repoman',
-    version='1.0',
+    version='1.1',
     description='RESTful repository management and buildbot for debian packages',
     author='Jeremy Grosser',
     author_email='synack@digg.com',
