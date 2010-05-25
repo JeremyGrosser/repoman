1016e1f45679aa74b7ec7f0f750174d3ef585c96
Jeremy Grosser <synack@digg.com>
2010-05-25 16:29:11 -0700

    Add a dependency on ncore and dependency_links

--- setup.py
+++ setup.py
@@ -9,4 +9,6 @@ setup(name='repoman',
     author_email='synack@digg.com',
     scripts=['scripts/repoman'],
     packages=['repoman'],
+    install_requires=['ncore>=1.6'],
+    dependency_links=['http://github.com/synack/ncore/downloads'],
 )
